4da9143e7ba6e32d7dbc489b81eaf58ae6ce505f
Jim Bumgardner <jbum@jbum.com>
2023-03-07 10:55:20 -0800

    Initial commit.

---
+++ app.arc
@@ -0,0 +1,12 @@
+@app
+covid19-lambda-publisher
+
+@http
+get /
+post /
+
+@aws
+profile ODI
+region us-west-1
+timeout 900
+architecture arm64
---
+++ src/http/get-index/index.js
@@ -0,0 +1,45 @@
+// learn more about HTTP functions here: https://arc.codes/http
+const { pantheonService } = require("@architect/shared/PantheonService");
+
+
+exports.handler = async function http(req) {
+
+  let message = "ERROR!";
+
+  try {
+    message = await pantheonService(req);
+  } catch (error) {
+    message = error;
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      "cache-control":
+        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+      "content-type": "text/html; charset=utf8",
+    },
+    body: `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sync cannabis.ca.gov content</title>
+  </head>
+  <!-- called via GET -->
+  <body class="padding-32">
+    <div class="max-width-320">
+      <div class="margin-left-8">
+        <div>
+          <code>
+            ${message}
+          </code>
+        </div>
+      </div>
+    </div>
+  </body>
+  </html>
+  `,
+  };
+}
